ac6a83f227c007746c85713052e1c3034b8d4981
PayMint_MJ <alswhddl10@naver.com>
2020-10-05 18:33:57 +0900

    Initial commit

---
+++ app/src/main/java/com/mj/aacsample/Room/Contact.kt
@@ -0,0 +1,25 @@
+package com.mj.aacsample.Room
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "contact")
+data class Contact (
+
+    @PrimaryKey(autoGenerate = true)
+    var id: Long?,
+
+    @ColumnInfo(name = "name")
+    var name: String,
+
+    @ColumnInfo(name = "number")
+    var number: String,
+
+    @ColumnInfo(name = "initial")
+    var inital: Char
+    ) {
+    constructor() : this(null, "", "", '\u0000')
+}
+
+//https://blog.yena.io/studynote/2019/03/27/Android-MVVM-AAC-2.html
